229b1f358445f007c19adf605d20de7ee4caabce
aaxlss <aaxlss@users.noreply.github.com>
2022-11-03 12:35:36 +0000

    deploy: 2e8ae1cd946c7f2b47ab40cfedc35351b84d57b2

---
+++ _dist_/index.js
@@ -0,0 +1,49 @@
+/**
+ * This file is just a silly example to show everything working in the browser.
+ * When you're ready to start on your site, clear the file. Happy hacking!
+ **/
+
+console.log('Happy hacking :)')
+const baseUrl ='https://platzi-avo.vercel.app/'
+const url = `${baseUrl}api/avo`;
+
+const appNode = document.querySelector('#app')
+
+const eventDelegation = (event) =>{
+    if(event.target.nodeName === 'H2'){
+        window.alert('Hi!')
+    }
+}
+
+appNode.addEventListener('click', eventDelegation)
+
+const formatPrice = (price) => {
+    const newPrice = new window.Intl.NumberFormat('en-EN',{
+        style:'currency',
+        currency:'USD'
+    }).format(price)
+    return newPrice
+}
+//web api
+window.fetch(url)
+.then(response => response.json())
+.then(data => {
+    let items = [];
+    data.data.forEach(({name, price, image}) => {
+        const img = document.createElement('img');
+        const title = document.createElement('h2');
+        const value = document.createElement('div')
+        //price information
+        value.textContent = formatPrice(price);
+        //title information
+        title.textContent = name;
+        // title.style.fontSize = '3rem'//setting up style using style attribute
+        title.className = 'text-2xl text-red-600'//setting up style  using classes from tailwind
+        //image information
+        img.src = `https://platzi-avo.vercel.app/${image}`;
+        const mainDiv = document.createElement('div')
+        mainDiv.append(title, img, value)
+        items.push(mainDiv)
+    });
+    appNode.append(...items)
+})
